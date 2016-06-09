6d87642b4b2af95ee2f5a9325ca304bfbccafc34
katieO <katajello@yahoo.com>
2016-06-09 14:39:22 -0700

    testing style index layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,15 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+
   end
 
   def create
+
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_session_path
+      session[:user_id] = @user.id
+      redirect_to styles_path
     end
   end
 
